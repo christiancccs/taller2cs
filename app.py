77c5a285ee560ef086441075d9a74b6cae57d731
christiancccs <christianccc2019@gmail.com>
2025-09-28 20:26:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Acontinuación se elaboró una tabla tipo Treemap con la ayuda del gr�
 
 tab2 = st.tabs(['Gráfico de análisis'])
 
-with Gráfico de análisis:
+with 'Gráfico de análisis':
     fig =  px.treemap(data_frame = apgn,
            path=[px.Constant("PGN"),
                  "Nombre Sector",
