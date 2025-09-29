c91f954148749e62f494a55f6d72fdf7e8ae100a
christiancccs <christianccc2019@gmail.com>
2025-09-28 20:50:53 -0500

    app.py

--- app.py
+++ app.py
@@ -6,11 +6,23 @@ apgn = pd.read_csv('datos_anteproyecto.csv')
 ran = pd.read_csv('datos_random.csv')
 
 st.title("Análisis de datos macroeconómicos")
-st.write("Acontinuación se elaboró una tabla tipo Treemap con la ayuda del gráficador rapido 'plotly.express' que permite visualizar los datos, anteriormente limpiados, en forma de gráfico jerárquico en el que cada rectángulo muestra la proyección de gasto de cada una de las dependencias del estado colombiano con base en el Programa Anteproyecto 2026 realizado por el Ministerio de Hacienda y Crédito Público de Colombia") 
+st.write("Acontinuación se elaboró una tabla tipo Treemap con la ayuda del gráficador rapido 'plotly.express' que permite visualizar los datos, anteriormente limpiados, en forma de gráfico jerárquico en el que cada rectángulo representa un elemento y el tamaño esta representado por un valor numérico.") 
 
 tab2, = st.tabs(['Gráfico de análisis'])
 
 with tab2:
+    st.subheader("Programa Anteproyecto 2026")
+
+st.write("" 
+         Este gráfico muestra la proyección presupuestal de cada una de las dependencias del estado colombiano con base en el Programa Anteproyecto 2026 realizado por el Ministerio de Hacienda y Crédito Público de Colombia el 4 de abril del 2025.
+
+         - El tamaño de cada rectangulo demuestra la proporción del presupuesto proyectado.
+         - Existen tres tipos de asignación presupuestal: 
+           A: Presupuesto de Funcionamiento.
+           B: Presupuesto de servicio de la deuda pública.
+           C: Presupuesto de inversión.
+        "") 
+        
     fig =  px.treemap(data_frame = apgn,
            path=[px.Constant("PGN"),
                  "Nombre Sector",
