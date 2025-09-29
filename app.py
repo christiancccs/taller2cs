c049580e4f7ad71e00163e548f08469d2a140be8
christiancccs <christianccc2019@gmail.com>
2025-09-28 20:28:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ ran = pd.read_csv('datos_random.csv')
 st.title("Análisis de datos macroeconómicos")
 st.write("Acontinuación se elaboró una tabla tipo Treemap con la ayuda del gráficador rapido 'plotly.express' que permite visualizar los datos, anteriormente limpiados, en forma de gráfico jerárquico en el que cada rectángulo muestra la proyección de gasto de cada una de las dependencias del estado colombiano con base en el Programa Anteproyecto 2026 realizado por el Ministerio de Hacienda y Crédito Público de Colombia") 
 
-tab2 = st.tabs(['Gráfico de análisis'])
+tab2, = st.tabs(['Gráfico de análisis'])
 
 with tab2:
     fig =  px.treemap(data_frame = apgn,
